39680a9781d38a27c1cc15bbd41a702c0ce91812
agent19music <seanmotanya@gmail.com>
2023-12-10 15:28:23 +0300

    Done with assignment

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,14 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self,string):
+        if isinstance (string,str):
+            self.knowledge.append(string)
+        else:
+            print("Error: Learn method requires a string")
+                
         pass
